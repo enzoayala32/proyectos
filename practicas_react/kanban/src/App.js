b14cde0c5b03bbba1a91ce92f29691fc966f489b
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-08-08 22:26:29 -0300

    inicio proyecto kanban UI

---
+++ practicas_react/kanban/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div style={{padding:"50px"}}>
+    <h1 style={{marginBottom:"20px"}}>Kanban UI</h1>
+   </div>
+  );
+}
+
+export default App;
